3ce5d0d983330de25908cc834033093bbe979591
Dimercel <xolcman@gmail.com>
2019-09-05 19:47:45 +0300

    Border limits

--- programm_003.hs
+++ programm_003.hs
@@ -13,3 +13,20 @@ randomPoints size max gen =
   let (first, newGen)   = randomR (0, max) gen
       (second, nextGen) = randomR (0, max) newGen
   in (first, second) : randomPoints (size - 1) max nextGen
+
+createLimits :: [Point] -> [Point]
+createLimits [] = []
+createLimits points =
+  let maxX = maximum $ map fst points
+      maxY = maximum $ map snd points
+      minX = minimum $ map fst points
+      minY = minimum $ map snd points
+      hLimit from to y = [(x, y) | x <- [from .. to]]
+      vLimit from to x = [(x, y) | y <- [from .. to]]
+  in concat [
+    hLimit (minX - 1) (maxX + 1) (minY - 1),
+    hLimit (minX - 1) (maxX + 1) (maxY + 1),
+    vLimit minY maxY (minX - 1),
+    vLimit minY maxY (maxX + 1),
+    points]
+
